9bdfdd36ab6d638128e54adc6d5c0344931f54bd
Wiktor Głodek <wiktor.glodek36@gmail.com>
2025-01-28 08:56:26 +0100

    szyfr cezara

---
+++ szyfr_cezara/szyfr_cezara.py
@@ -0,0 +1,14 @@
+def caesar_cipher(text, shift):
+    encrypted = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            encrypted += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            encrypted += char
+    return encrypted
+
+
+text = "ADAM"
+shift = 3
+print(caesar_cipher(text, shift))
